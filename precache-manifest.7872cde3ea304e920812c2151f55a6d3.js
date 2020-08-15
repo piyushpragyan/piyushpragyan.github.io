98a9dbaf80bbcbf2cddbe21eac0395da86da3e3b
Piyush Pragyan <piyushpragyan14@gmail.com>
2020-08-15 13:37:57 +0530

    Updates

---
+++ precache-manifest.7872cde3ea304e920812c2151f55a6d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5635c45df2b0107fbcf26135c2ec22f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2d51f18a141988200df",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "496cb27cd1158f6323e8",
+    "url": "/static/js/2.7db99099.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7db99099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d51f18a141988200df",
+    "url": "/static/js/main.df7c6396.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b98c1b34e4f583dcc42f74a7d48f217f",
+    "url": "/static/media/download.b98c1b34.png"
+  }
+]);
